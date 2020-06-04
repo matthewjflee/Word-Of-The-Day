451183ccc20ccd4387cc11c47a3b11011880093f
matthewjflee <matthewjflee@outlook.com>
2020-06-04 17:03:55 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Word Of The Day
+
+This is a python script to grab Dictionary.com's word of the day and display it on the terminal.
+
+![](./img/2020-06-04-17-03-00.png)<br/>
---
+++ wotd.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+from os import path
+
+"""
+This script will go to dictionary.com's word of the day and display the word on the console
+
+Author: matthewjflee
+"""
+
+class colors:
+	HEADER = '\033[95m'
+	CYAN = '\033[36m'
+	BLUE = '\033[34m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+	ITALIC = '\033[3m'
+	DIM = '\033[2m'
+
+def word_of_the_day():
+	# Get the page
+	URL 	= 'https://www.dictionary.com/e/word-of-the-day/'
+	headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0'}
+	page	= requests.get(URL, headers = headers)
+	soup 	= BeautifulSoup(page.content, 'html.parser')
+
+	# Word and definitions
+	word 	   = soup.find('div', class_ = 'wotd-item-headword__word').find('h1').get_text()
+	pronounce  = soup.find('div', class_ = 'wotd-item-headword__pronunciation').find('div').get_text().strip()
+	word_div   = soup.find('div', class_ = 'wotd-item-headword__pos').findAll('p')
+	word_type  = word_div[0].get_text().strip()
+	definition = word_div[1].get_text()
+
+	# Print to console
+	line1 = f"{colors.CYAN}{colors.BOLD}{word}{colors.ENDC} {pronounce}: {colors.ITALIC}{word_type}"
+	line2 = f"{definition}"
+
+	print(line1)
+	print(line2)
+	
+if __name__ == "__main__":
+	word_of_the_day() 
